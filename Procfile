d5efd1e068e4dca9dee14a6a68196aa4599f4749
Alex Perkowski <perkowskialex@gmail.com>
2020-01-22 17:04:10 -0600

    center add maintenance form

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js && npm run build
+web: node server.js
--- src/pages/AddMaintenancePage/AddMaintenancePage.jsx
+++ src/pages/AddMaintenancePage/AddMaintenancePage.jsx
@@ -32,56 +32,68 @@ export default class AddMaintenancePage extends Component {
   render() {
     return (
       <>
-        <h1>Add a Maintenance</h1>
-        <form
-          ref={this.formRef}
-          autoComplete="off"
-          onSubmit={this.handleSubmit}
+        <h2 className="center">Add a Maintenance</h2>
+        <div
+          className="center"
+          style={{
+            display: "block",
+            textAlign: "center",
+            marginLeft: "33%"
+          }}
         >
-          <div>
-            <label>Maintenance Name</label>
-            <input
-              name="name"
-              value={this.state.formData.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Maintenance Description</label>
-            <input
-              name="description"
-              value={this.state.formData.description}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Time to Complete (in days)</label>
-            <input
-              name="time"
-              value={this.state.formData.time}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Location</label>
-            <input
-              name="location"
-              value={this.state.formData.location}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn"
-            disabled={this.state.invalidForm}
+          <form
+            className="center"
+            style={{ width: "50%", alignItems: "center" }}
+            ref={this.formRef}
+            autoComplete="off"
+            onSubmit={this.handleSubmit}
           >
-            ADD MAINTENANCE
-          </button>
-        </form>
+            <div>
+              <label>Maintenance Name</label>
+              <input
+                name="name"
+                value={this.state.formData.name}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Maintenance Description</label>
+              <input
+                name="description"
+                value={this.state.formData.description}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Time to Complete (in days)</label>
+              <input
+                name="time"
+                value={this.state.formData.time}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Location</label>
+              <input
+                name="location"
+                value={this.state.formData.location}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn waves-effect waves-light blue darken-1"
+              disabled={this.state.invalidForm}
+            >
+              ADD MAINTENANCE<i class="material-icons left">add</i>
+            </button>
+          </form>
+        </div>
+        &nbsp;
       </>
     );
   }
